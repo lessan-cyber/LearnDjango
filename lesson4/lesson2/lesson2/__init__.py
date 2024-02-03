a46b8980075b4ad6d722cf6e4cb9cb73f473007e
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2024-02-03 22:23:30 +0000

    add lesson 4

--- lesson4/lesson2/lesson2/__init__.py
+++ lesson4/lesson2/lesson2/__init__.py
@@ -1,3 +1,3 @@
 # pour la connection avec la base de donner mysql
-import pymysql
-pymysql.install_as_MySQLdb()
+"""import pymysql
+pymysql.install_as_MySQLdb()"""
--- lesson4/lesson2/posts/models.py
+++ lesson4/lesson2/posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
